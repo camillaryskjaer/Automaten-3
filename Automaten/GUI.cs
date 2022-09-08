5cfb224c7f12c4dad6162aa4e55b21a721e63522
Simon Onyszczak Hansen <simo401t@zbc.dk>
2022-09-08 12:54:51 +0200

    Add project files.

---
+++ Automaten/GUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaten
+{
+    internal class GUI
+    {
+        public void Menu()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------\n");
+            Console.WriteLine("                                    Vending machine");
+            Console.WriteLine("\n---------------------------------------------------------------------------------");
+            Console.WriteLine("1. Cola");
+            Console.WriteLine("2. Mars bar");
+            Console.WriteLine("3. Fanta");
+            Console.WriteLine("4. sprite");
+            Console.WriteLine("5. BBQ Chips");
+            Console.WriteLine("6. Snickers");
+        }
+        public void Test()
+        {
+            
+        }
+    }
+}
---
+++ Automaten/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Automaten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GUI gui = new GUI();
+            gui.Menu();
+            ConsoleKeyInfo key;
+
+           
+
+        }
+    }
+}
